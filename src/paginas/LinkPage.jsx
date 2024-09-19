3bb836d09fd52f01fb49f6a81e32b7de198f03cc
alexandrenf <ale.borges8@me.com>
2024-09-19 14:06:47 +0000

    bug fixes

--- src/paginas/LinkPage.jsx
+++ src/paginas/LinkPage.jsx
@@ -25,10 +25,12 @@ const LinkTreePage = () => {
       <LinkButton name="Site da IFMSA Brazil" link="https://ifmsabrazil.org" />
       <LinkButton name="Instagram" link="https://instagram.com/ifmsabrazil" />
       <LinkButton name='Instagram de Intercâmbios Internacionais' link='https://instagram.com/ifmsabrazilexchanges' />
-      <LinkButton name="Instagram de Intercâmbios Nacionais" link="https://instagram.com/ifmsabrazilintercamnbios" />
+      <LinkButton name="Instagram de Intercâmbios Nacionais" link="https://instagram.com/ifmsabrazilintercambios" />
       <LinkButton name="BMS Journal Instagram" link="https://instagram.com/bmsjournal" />
       <LinkButton name="Declarações de Política" link="https://ifmsabrazil.org/arquivos/declaracoes-de-politica" />
-      <LinkButton name="Em Breve" link="#" />
+      <LinkButton name="Como submeter à BMS" link="https://drive.google.com/drive/u/0/mobile/folders/1k1PsgHNX-pwwZbzuZWrd2kV3CRsaWv4Z?usp=sharing" />
+      <LinkButton name="Introdução à BMS" link="https://drive.google.com/file/d/17RMM6f24aj6QnFgmnP4Wa-IDakiNqI4q/view?usp=drivesdk" />
+      <LinkButton name="Chamada Ressonância Poética" link="https://ifmsabrazil.org/arquivo/5/chamada-para-submissao-de-trabalhos-no-v-resson-ncia-poetica" />
       <LinkButton name="Em Breve" link="#" />
     </LinkTreeContainer>
   );
