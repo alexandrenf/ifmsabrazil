b4ce99fb40fc0a7fde242d5ee0483d8ef07ab029
Alex <5628895+alexandrenf@users.noreply.github.com>
2025-03-24 22:59:55 -0300

    update vite version and remove unused particle animation

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -88,17 +88,6 @@ const glowPulse = keyframes`
   }
 `
 
-const particleFloat = keyframes`
-  0% {
-    transform: translateY(0) translateX(0) rotate(0);
-    opacity: 1;
-  }
-  100% {
-    transform: translateY(-100px) translateX(20px) rotate(180deg);
-    opacity: 0;
-  }
-`
-
 const rotatePattern = keyframes`
   0% {
     transform: rotate(0deg);
@@ -214,31 +203,6 @@ const HeroSection = styled.div`
     opacity: 0.8;
     filter: brightness(0.7) contrast(1.2);
   }
-`
-
-const Particles = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-
-  &::before, &::after {
-    content: '';
-    position: absolute;
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background-color: rgba(255, 255, 255, 0.5);
-  }
-
-  ${[...Array(20)].map((_, i) => `
-    &::before:nth-child(${i}) {
-      left: ${Math.random() * 100}%;
-      top: ${Math.random() * 100}%;
-      animation-delay: ${Math.random() * 4}s;
-      animation-duration: ${3 + Math.random() * 2}s;
-    }
-  `)}
 `
 
 const HeroContent = styled.div`
@@ -610,7 +574,6 @@ const Home = () => {
         />
       )}
       <HeroSection>
-        <Particles />
         <HeroContent>
           <HeroText>
             Estudantes de medicina que fazem a <span>diferença</span>
